56fcdda236e0fc246dd6da0a865b5f205c0affd8
sharifmb92-bit <sharif.mb92@gmail.com>
2026-04-19 15:30:59 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-const CACHE_NAME = 'bus-v1';
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(['./', 'index.html'])));
-});
-self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
-});
